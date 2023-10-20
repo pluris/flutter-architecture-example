b0f6e0a6cf593880260c8708707ac9d85bcc1e3c
pluris <zighart8456@naver.com>
2023-10-20 17:19:57 +0900

    implement remote_character_state

--- lib/features/presentation/bloc/character/remote/remote_character_state.dart
+++ lib/features/presentation/bloc/character/remote/remote_character_state.dart
@@ -1,15 +1,26 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter_architecture_example/features/domain/entity/character_entity.dart';
 
 enum RemoteCharactersStatus { loading, success, failed }
 
 class RemoteCharactersState extends Equatable {
+  final RemoteCharactersStatus status;
+  final List<CharacterEntity> characters;
+  final bool hasMaxReached;
 
-  const RemoteCharactersState();
+  const RemoteCharactersState({
+      this.status = RemoteCharactersStatus.loading,
+      this.characters = const <CharacterEntity>[],
+      this.hasMaxReached = false});
 
-  RemoteCharactersState copyWith() {
-    return const RemoteCharactersState();
+  RemoteCharactersState copyWith(
+      {RemoteCharactersStatus? pStatus, List<CharacterEntity>? pCharacters, bool? pHasMaxReached}) {
+    return RemoteCharactersState(
+        status: pStatus ?? status,
+        characters: pCharacters ?? characters,
+        hasMaxReached: pHasMaxReached ?? hasMaxReached);
   }
 
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [status, characters, hasMaxReached];
 }
