032a7068d0517e7569cc2f9956edc034665dad1c
pluris <zighart8456@naver.com>
2023-10-20 17:00:49 +0900

    add character_entity into domain

---
+++ lib/features/domain/entity/character_entity.dart
@@ -0,0 +1,61 @@
+import 'package:floor/floor.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'character_entity.freezed.dart';
+part 'character_entity.g.dart';
+
+@freezed
+class CharacterResponse with _$CharacterResponse {
+  const factory CharacterResponse({
+    required Info info,
+    required List<CharacterEntity> results,
+  }) = _CharacterResponse;
+
+  factory CharacterResponse.fromJson(Map<String, dynamic> json) =>
+      _$CharacterResponseFromJson(json);
+}
+
+@freezed
+class Info with _$Info {
+  const factory Info({
+    required int count,
+    required int pages,
+    required String? next,
+    required String? prev,
+  }) = _Info;
+
+  factory Info.fromJson(Map<String, dynamic> json) => _$InfoFromJson(json);
+}
+
+@freezed
+@Entity(tableName: 'character', primaryKeys: ['id'])
+abstract class CharacterEntity with _$CharacterEntity {
+    const factory CharacterEntity({
+    int? id,
+    String? name,
+    String? status,
+    String? species,
+    String? type,
+    String? gender,
+    String? image,
+    List<String>? episode,
+    String? url,
+    DateTime? created,
+  }) = _CharacterEntity;
+
+  factory CharacterEntity.fromJson(Map<String, dynamic> map) =>
+      _$CharacterEntityFromJson(map);
+    factory CharacterEntity.fromEntity(CharacterEntity entity) =>
+      CharacterEntity(
+        id: entity.id,
+        name: entity.name,
+        status: entity.status,
+        species: entity.species,
+        type: entity.type,
+        gender: entity.gender,
+        image: entity.image,
+        episode: entity.episode,
+        url: entity.url,
+        created: entity.created,
+      );
+}
